83e9e1ba624cd21ef37176cf38fb0e1aefed5582
KunSung <tttzkq9336@gmail.com>
2023-12-28 17:49:54 +0900

    first commit.

---
+++ fisa_of_candy/src/main/java/domain/model/User.java
@@ -0,0 +1,21 @@
+package domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class User {
+
+	private long id;
+	private String name;
+	private String pw;
+	private long targetId;
+
+}
